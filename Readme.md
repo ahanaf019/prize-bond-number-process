9f2321676c090e9f99ce1c4a6014d8f34d553e74
Md. Ahanaf Arif Khan <ahanaf019@gmail.com>
2023-02-19 09:46:18 +0600

    Add Readme.md and support for command line arguments.

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Prize Bond Number Processor
+The program takes input from a .txt file in the format below:
+
+```
+<!-- input.txt -->
+STARTING_1 NUMBER_OF_BONDS
+STARTING_2 NUMBER_OF_BONDS
+STARTING_3 NUMBER_OF_BONDS
+STARTING_4 NUMBER_OF_BONDS
+```
+
+The input file is processed and a string is generated with the number ranges which is accepted by [Bangladesh Bank](https://www.bb.org.bd/en/index.php/investfacility/prizebond)'s Winning Bond Searching page. The program outputs to the console and an `out.txt` file.
+
+# Usage
+```
+python3 process.py FILENAME.txt
+```
--- process.py
+++ process.py
@@ -1,32 +1,78 @@
+import sys
+import os
+import pathlib
 
-filename = 'test.txt'
-with open(FILENAME, mode='r') as file:
-    
-    lines = file.readlines()
-    # print(lines)
-    sum = 0
-    for line in lines:
+
+def process(filename: str):
+
+    with open(filename, mode='r') as file:
         
-        first = line.split(' ')
-        second = first[1]
-        first = first[0]
-        # print(first, int(second))
-        
-        start = int(first)
-        end = start + int(second) - 1
-        sum += int(second)
-        end = str(end)
-        
-        start = str(start)
-        n = len(first) - len(start)
-        start = '0'*n + start
-        end = '0'*n + end
-        
-        if int(second) == 1:
-            print(f'{start},', end='')
-        else:
-            print(f'{start}~{end},', end='')
+        with open('out.txt', mode="w") as wfile:
+            lines = file.readlines()
+            # print(lines)
+            sum = 0
+            try:
+                for line in lines:
+                    
+                    first = line.split(' ')
+                    second = first[1]
+                    first = first[0]
+                    # print(first, int(second))
+                    
+                    start = int(first)
+                    end = start + int(second) - 1
+                    sum += int(second)
+                    end = str(end)
+                    
+                    start = str(start)
+                    n = len(first) - len(start)
+                    start = '0'*n + start
+                    end = '0'*n + end
+                    
+                    if int(second) == 1:
+                        wfile.write(f'{start},')
+                        print(f'{start},', end='')
+                    else:
+                        wfile.write(f'{start}~{end},')
+                        print(f'{start}~{end},', end='')
+                        
+                print()
+                print()
+                print(f'Total: {sum}, {sum*100}')
             
-    print()
-    print()
-    print(f'Total: {sum}, {sum*100}')
+            except Exception as e:
+                print_usage("INVALID FILE CONTENTS")
+
+
+def print_usage(err: str):
+    msg = f'''{err}
+    
+Usage:
+        python3 {sys.argv[0]} FILENAME.txt
+    '''
+    print(msg)
+
+
+def main():
+    if(len(sys.argv) != 2):
+        print_usage('INVALID ARGUMENTS!')
+        return
+    
+    filename = sys.argv[1]
+    
+    if filename.split('.')[1] != 'txt':
+        print_usage('INVALID FILETYPE. ".txt" file required.')
+        return
+    
+    path = pathlib.Path(filename)
+    if path.is_file():
+        process(filename)
+        
+    else:
+        print_usage("NO FILE FOUND OR INVALID FILE.")
+    
+
+
+
+if __name__ == "__main__":   
+        main()
